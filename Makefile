7b6f3104203075917a747254bcf28ab099b40d69
Abe <abe149@gmail.com>
2020-06-10 10:33:25 -0400

    lots of progress in/on "/compile.sh"

--- Makefile
+++ Makefile
@@ -33,7 +33,7 @@ build_dir./FS_CryptoTest: build_dir. $(SOURCE_FILENAME)
 #	echo INFO: CXX=$(CXX)
 #	echo INFO: CXX_IS_COMPATIBLE_WITH_GCC_FLAGS=$(CXX_IS_COMPATIBLE_WITH_GCC_FLAGS)
 	# $(CXX) $(CXXFLAGS) $(SOURCE_FILENAME) -o build_dir./$(EXECUTABLE_BASENAME) # embedded assumption: the compiler`s driver "understands" "-o <...>" to mean "output pathname"
-	./compile.sh $(SOURCE_FILENAME) build_dir./$(EXECUTABLE_BASENAME)
+	./compile.sh $(SOURCE_FILENAME) build_dir./$(EXECUTABLE_BASENAME) $(CXX) # CXX comes _last_ here on _purpose_ in case it`s not set
 
 
 
--- compile.sh
+++ compile.sh
@@ -1,6 +1,22 @@
 #!/usr/bin/env sh
-echo "--- in ''$0'' ---"
-echo "--- ''\$@'' :''$@'' ---"
-echo "--- ''\$1'' :''$1'' ---"
-echo "--- ''\$2'' :''$2'' ---"
+echo "--- INFO: in ''$0'': ---"
+echo "--- INFO:   ''\$@'' :[$@] ---"
+echo "--- INFO:   ''\$1'' :''$1'' ---"
+echo "--- INFO:   ''\$2'' :''$2'' ---"
+echo "--- INFO:   ''\$3'' :''$3'' ---"
 
+compiler=`which "$3"`
+if test \( -z "$compiler" \) -o ! \( -x "$compiler" -a ! -d "$compiler" \); then # if the alleged compiler arg. is not provided, or points to something not executable or a directory
+  echo '--- INFO:   Going to try to autodetect the C++ compiler command. ---'
+else
+  echo "--- INFO:   Using provided compiler command ''$3'', found at ''$compiler''. ---"
+fi
+
+if test \( -z "$compiler" \) -o ! \( -x "$compiler" -a ! -d "$compiler" \); then
+  echo "--- ERROR: No valid compiler command found at ''$compiler''.  Aborting. ---"
+  exit 1 # TO DO: add anti-sourcing protection.
+fi
+
+# embedded assumption: the compiler`s driver "understands" "-o <...>" to mean "output pathname"
+$compiler "$1" -o "$2"
+echo "Compiler exit/result code: $?"
