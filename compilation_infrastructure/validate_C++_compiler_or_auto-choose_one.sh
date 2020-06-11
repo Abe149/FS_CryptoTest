75d66897d34da3a551b4ec056a084ce70076d8b2
Abe <abe149@gmail.com>
2020-06-11 12:18:04 -0400

    made "validate_C++_compiler_or_auto-choose_one.sh" do its job _very_ well, incl. torture-testing the code that tries to pick a fallback if/when the input is invalid [incl. the empty case, i.e. no input at all]; the only thing that this script does not yet "do what it says on the tin" is require an actual _C++_ compiler, i.e. as of now it will accept a C-only compiler too

--- compilation_infrastructure/validate_C++_compiler_or_auto-choose_one.sh
+++ compilation_infrastructure/validate_C++_compiler_or_auto-choose_one.sh
@@ -8,19 +8,53 @@ Q_and_D_readlink_substitute_needed_due_to_lack_of_readlink_in_POSIX() {
 }
 . $(dirname "`Q_and_D_readlink_substitute_needed_due_to_lack_of_readlink_in_POSIX "$0"`")/shared_functions.sh
 
+
+
+test_alleged_Cxx_compiler() {
+  ### <https://pubs.opengroup.org/onlinepubs/9699919799/utilities/date.html>
+  ### the next line`s finely-tuned monstrosity is due to the lack of a suitable "make temporary file" shell API in POSIX version 2008 and later, and is carefully designed to avoid ASCII {spaces and colons}
+  test_file_base_pathname=/tmp/C++_compiler_validation_test_started_at___`date '+%a_%A_%b_%B_%C_%d_%H_%I_%j_%m_%M_%p_%S_%u_%U_%V_%w_%W_%y_%Y_%Z'`
+  sleep 1 # unfortunately, POSIX "date" doesn`t seem to provide a {milli/micro/nano}seconds percent-prefixed character API, so we need to make sure that subsequent calls to this function in the same execution don`t execute within the same second
+
+  source_extension=.cpp
+  executable_extension=.exe # should work fine on Unix, even though not required "there"
+
+  if [ -e "$test_file_base_pathname""$source_extension" -o -e "$test_file_base_pathname""$executable_extension" ]; then
+    echo "FATAL ERROR: either ''$test_file_base_pathname""$source_extension'' or ''$test_file_base_pathname""$executable_extension'' or both already existed."
+    exit 1 # TO DO: add anti-sourcing protection
+  fi
+
+  # TO DO: make the next line require a real C++ compiler, i.e. not also accept a C-only compiler
+  echo 'int main(){}' > "$test_file_base_pathname""$source_extension" # the most-concise C++ program I know, albeit also a valid [and also the most-concise I know] _C_ program
+  # EMBEDDED ASSUMPTION on the next line: "-o" _always_ means "output to the following pathname"
+  "$1" "$test_file_base_pathname""$source_extension" -o "$test_file_base_pathname""$executable_extension" >&2
+  #                ensure any stdout output from the compiler doesn`t pollute this script`s stdout output ^^^
+
+  # EMBEDDED ASSUMPTIONS on the next line: [1] the "file" utility can identify the executable format; [2] the "file" utility`s output for the executable format in use includes the word "executable"
+  if file "$test_file_base_pathname""$executable_extension" | grep -iq executable; then
+    return 0 # reminder: 0 means "OK" in the context of Unix shell scripting
+  else
+    return 9
+  fi
+
+  # possible TO DO: should we remove the temporary file[s], assuming we successfully created it/them?
+}
+
 ### --- ^^^ --- functions --- ^^^ --- ###
 
 
 
 compiler_command=`which "$1"` # NOTE: simple "$1": no prefix, no nothing
-if test -n "$compiler_command" && is_executable_and_not_a_directory "$compiler_command"; then # if the alleged compiler arg. is not provided, or points to something not executable or a directory
-  stderr_echo "--- INFO:   Using provided compiler command ''$3'', found at ''$compiler_command''. ---"
+if test -n "$compiler_command" && is_executable_and_not_a_directory "$compiler_command" && test_alleged_Cxx_compiler "$compiler_command"; then # if the alleged compiler arg. is not provided, or points to something not executable or a directory
+  stderr_echo "--- INFO:   Using provided compiler command ''$1'', found at ''$compiler_command''. ---"
 else
   stderr_echo '--- INFO:   Going to try to autodetect the C++ compiler command. ---'
   old_compiler_command="$compiler_command"
-  for alleged_compiler_command in c++ CC g++ clang++; do
+
+# for alleged_compiler_command in INTENTIONALLY-INVALID_COMMAND_NAME ls c++ CC g++ clang++; do # torture-testing version of the "for" loop header
+  for alleged_compiler_command in                                       c++ CC g++ clang++; do
     alleged_compiler_fullPath=`which $alleged_compiler_command`
-    if is_executable_and_not_a_directory "$alleged_compiler_fullPath"; then
+    if is_executable_and_not_a_directory "$alleged_compiler_fullPath" && test_alleged_Cxx_compiler "$alleged_compiler_fullPath"; then
       compiler_command="$alleged_compiler_fullPath"
       break
     fi
